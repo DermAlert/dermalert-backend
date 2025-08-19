10dfa27ff210bc158aaf90bb8300d009a361b032
Giovanni Giampauli <giovanni.acg@gmail.com>
2025-08-19 11:46:33 -0300

    feat: substituir referência direta ao modelo de usuário pelo uso de settings.AUTH_USER_MODEL

--- profile_forms/models/base_form.py
+++ profile_forms/models/base_form.py
@@ -1,5 +1,6 @@
 from core.models import BaseModel
 from django.db import models
+from django.conf import settings
 
 
 class BaseForm(BaseModel):
@@ -10,7 +11,7 @@ class BaseForm(BaseModel):
     id = models.AutoField(primary_key=True)
     description = models.TextField(blank=True, null=True)
     user = models.ForeignKey(
-        "auth.User",
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name="profile_forms",
         blank=True,
